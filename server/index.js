4fb4b025a78ed624bf10f8d5146dc7e0c0c54b59
Preet1810 <preetpalsingh1239@gmail.com>
2023-02-27 16:32:44 +0530

    -

--- server/index.js
+++ server/index.js
@@ -8,8 +8,6 @@ import multer from "multer";
 import helmet from "helmet";
 import morgan from "morgan";
 //NATIVE IMPORTS 
-import path from "path";
-import { fileURLToPath } from "url";
 
 import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/users.js"
@@ -17,12 +15,9 @@ import postRoutes from "./routes/posts.js"
 import { register } from "./controllers/auth.js";
 import { createPost } from "./controllers/posts.js";
 import { verifyToken } from "./middleware/auth.js";
-import User from "./models/User.js";
-import Post from "./models/Post.js";
-import { users, posts } from "./data/index.js"
+import { cloudinary, storage } from './cloudinary/index.js';
+
 /* CONFIGURATIONS */
-const __filename=fileURLToPath(import.meta.url);
-const __dirname=path.dirname(__filename);
 dotenv.config();
 const app=express();
 app.use(express.json());
@@ -32,18 +27,7 @@ app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
-app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
-/* FILE STORAGE */
-// const storage=multer.diskStorage({
-//     destination: function (req, file, cb) {
-//         cb(null, "public/assets");
-//     },
-//     filename: function (req, file, cb) {
-//         cb(null, file.originalname);
-//     },
-// });
-import { cloudinary, storage } from './cloudinary/index.js';
 
 
 const upload=multer({ storage });
@@ -51,9 +35,7 @@ const upload=multer({ storage });
 /* ROUTES WITH FILES */
 app.post("/auth/register", upload.single("picture"), register);
 app.post("/posts", verifyToken, upload.single("picture"), createPost);
-// app.get('/', (req, res) => {
-//     res.send('hey');
-// })
+
 /* ROUTES */
 app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
@@ -67,9 +49,5 @@ mongoose
     })
     .then(() => {
         app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
-
-        /* ADD DATA ONE TIME */
-        // User.insertMany(users);
-        // Post.insertMany(posts);
     })
     .catch((error) => console.log(`${error} did not connect`));
