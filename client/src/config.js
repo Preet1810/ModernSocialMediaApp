3632199c5aa6f83eb18db37db5eac6f930d85feb
Preet1810 <preetpalsingh1239@gmail.com>
2023-02-27 20:55:07 +0530

    client-url

--- client/src/config.js
+++ client/src/config.js
@@ -1,6 +1,6 @@
 const config={
-    // API_BASE_URL: 'https://memories-server-edyz.onrender.com', //production
-    API_BASE_URL: 'http://localhost:3001' //development
+    API_BASE_URL: 'https://memories-server-edyz.onrender.com', //production
+    // API_BASE_URL: 'http://localhost:3001' //development
 
 };
 
--- server/index.js
+++ server/index.js
@@ -26,7 +26,10 @@ app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors());
+app.use(cors({
+    origin: process.env.CLIENT_URL,
+    optionsSuccessStatus: 200
+}));
 
 
 
