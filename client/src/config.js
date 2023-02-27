79b3a5722cc2e6254e4e572cf4ea6b5c3b5ad379
Preet1810 <preetpalsingh1239@gmail.com>
2023-02-27 20:54:14 +0530

    client-url

--- client/src/config.js
+++ client/src/config.js
@@ -1,5 +1,7 @@
 const config={
-    API_BASE_URL: 'https://memories-server-edyz.onrender.com',
+    // API_BASE_URL: 'https://memories-server-edyz.onrender.com', //production
+    API_BASE_URL: 'http://localhost:3001' //development
+
 };
 
 export default config;
