8530b56bcb02b95f10d918d6097dcbff3374893f
Preet1810 <preetpalsingh1239@gmail.com>
2023-02-27 19:47:21 +0530

    client-url

--- server/index.js
+++ server/index.js
@@ -27,7 +27,7 @@ app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors({
-    origin: '*',
+    origin: process.env.CLIENT_URL,
     optionsSuccessStatus: 200
 }));
 
